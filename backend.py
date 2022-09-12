99a88921d6ee56c31ad0c88508233a08c300b0f3
L-Andrei <101563044+L-Andrei@users.noreply.github.com>
2022-09-12 09:09:48 -0300

    Update backend.py

--- backend.py
+++ backend.py
@@ -4,11 +4,13 @@ from modelo import *
 
 @app.route('/')
 def iniciar_backend():
+    # Inicia o backend.
     a = 'Backend iniciado!!!'
     return a
 
 @app.route('/listar')
 def listar_registros():
+    #Lista os registros a partir do 5000.
     select =  db.session.query(Compania).offset(5000).limit(100)
     resposta = [ x.json() for x in select ]
     resposta = jsonify(resposta)
@@ -17,6 +19,7 @@ def listar_registros():
 
 @app.route('/quantos_registros')  
 def retorna_quantidade():
+    #Retorna quantos registros exitem no banco de dados.
     resultado = len(db.session.query(Compania.id).all())
     return str(resultado)
 
